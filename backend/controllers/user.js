38df8abb304f3e42663eb2d40bc154200dece4bc
Ilya Petrov <dnamix1@gmail.com>
2020-01-16 13:51:42 +0300

    regular (not admin) user profile update functionality implemented on the back-end with appropriate routes and controller methods

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const lodash = require("lodash");
+const formidable = require("formidable");
+const fs = require("fs");
 
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
@@ -36,6 +39,53 @@ exports.publicProfile = (req, res) => {
             blogs: data
           });
         });
+    }
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Photo can not be uploaded..."
+      });
+    }
+    let user = req.profile;
+    user = _.extend(user, fields);
+
+    if (files.photo) {
+      if (files.photo.size > 100000) {
+        return res.status(400).json({
+          error: "Photo exceeds 1Mb...Compress or choose lesser sized image"
+        });
+      }
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+      user.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        user.hashed_password = undefined;
+        res.json(user);
+      });
+    }
+  });
+};
+
+exports.photo = (req, res) => {
+  const username = req.params.username;
+  User.findOne({ username }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found..."
+      });
+    }
+    if (user.photo.data) {
+      res.set("Content-Type", user.photo.contentType);
+      return res.send(user.photo.data);
     }
   });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const {
   requireSignin,
   authMiddleware,
-  adminMiddleware
+  adminMiddleware,
+  update,
+  photo
 } = require("../controllers/auth");
 const { read, publicProfile } = require("../controllers/user");
 
 router.get("/profile", requireSignin, authMiddleware, read);
 router.get("/user/:username", publicProfile);
+router.put("/user/update", requireSignin, authMiddleware, update);
+router.get("/user/photo/:username", photo);
 
 module.exports = router;
