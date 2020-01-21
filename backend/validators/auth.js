e75540429e1b92ecdb54058fcbc14ce8ed0f5793
Ilya Petrov <dnamix1@gmail.com>
2020-01-21 11:05:06 +0300

    styled home page and created validators for restore password

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -21,3 +21,19 @@ exports.userSigninValidator = [
     .isLength({ min: 6 })
     .withMessage("Password can't be shorter than 6 characters...")
 ];
+
+exports.forgotPasswordValidator = [
+  check("email")
+    .not()
+    .isEmpty()
+    .isEmail()
+    .withMessage("Email address is incorrect...")
+];
+
+exprorts.resetPasswordValidator = [
+  check("newPassword")
+    .not()
+    .isEmpty()
+    .isLength({ min: 6 })
+    .withMessage("Password can't be shorter than 6 characters...")
+];
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,11 +4,20 @@ import Link from "next/link";
 const Index = () => {
   return (
     <Layout>
-      <main style={{ paddingTop: "100px" }} className="text-center">
-        <div style={{ homeBackground }}>
-          <h2>Home Page</h2>
-          <Link href="/signup">
-            <a>Sign Up</a>
+      <main className="text-center">
+        <div style={homeBackground}>
+          <div
+            style={{
+              position: "relative",
+              top: "30vh",
+              color: "yellow",
+              fontSize: "4rem"
+            }}
+          >
+            Ultimate Blogging
+          </div>{" "}
+          <Link href="/signin">
+            <a style={{ fontSize: "2rem", color: "white" }}>Sign In</a>
           </Link>
         </div>
       </main>
@@ -17,10 +26,17 @@ const Index = () => {
 };
 
 const homeBackground = {
+  // backgroundImage: "url('../public/images/blogs.jpeg)",
+  width: "100vw",
+  height: "100%",
+  // backgroundRepeat: "no-repeat",
+  // backgroundSize: "cover"
   background:
-    "linear-gradient(328.78deg, #030086 14.45%, #BD6177 84.36%), linear-gradient(301.28deg, #209B4A 0%, #7000FF 100%), radial-gradient(100% 138.56% at 100% 0%, #D50000 0%, #00FFE0 100%), radial-gradient(100% 148.07% at 0% 0%, #D50000 0%, #00FFFF 100%)",
-  backgroundBlendMode: "soft-light, overlay, difference, normal",
-  paddingTop: "100px"
+    "linear-gradient(235deg, #FFFFFF 0%, #000F25 100%), linear-gradient(180deg, #6100FF 0%, #000000 100%), linear-gradient(235deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%), linear-gradient(125deg, #FFA3AC 0%, #FFA3AC 40%, #00043C calc(40% + 1px), #00043C 60%, #005D6C calc(60% + 1px), #005D6C 70%, #00C9B1 calc(70% + 1px), #00C9B1 100%)",
+  backgroundBlendMode: "soft-light, screen, darken, normal",
+  position: "absolute",
+  display: "flex",
+  flexDirection: "column"
 };
 
 export default Index;
